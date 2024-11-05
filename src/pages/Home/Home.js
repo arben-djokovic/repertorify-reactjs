2630c584a8432409c383f26b68805ce54d3e7785
arben-djokovic <benidjokovic12@gmail.com>
2024-11-05 13:33:24 +0100

    nothing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   // Create a ref for the Swiper instance
